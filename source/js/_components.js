6c7a4cd4f63f22a1c7031b1b34bc2ee43ffca9a6
roman <romasaab@gmail.com>
2024-03-12 20:33:09 +0200

    fix

--- source/js/_components.js
+++ source/js/_components.js
@@ -1,10 +1,4 @@
 import './components/sliders';
-// import './components/burger';
-// import './components/getDinamicHeight';
-// import './components/accordions';
-// import './components/replaceEl';
-// import './components/modals';
-// import './components/aos';
 
 
 
